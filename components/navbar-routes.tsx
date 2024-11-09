04684f083757efd63c29084bc2b03baaff8f7ec3
Rituraj <singhrituraj783@gmail.com>
2024-11-10 04:28:08 +0530

    added-Course-Chapters-Layout

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,6 +1,4 @@
 "use client";
-import { SearchInput } from "./search-input";
-
 
 import { UserButton} from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
@@ -10,31 +8,32 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 // import { isTeacher } from "@/lib/teacher";
 
+import { SearchInput } from "./search-input";
 
 export const NavbarRoutes = () => {
  
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
-  const isPlayerPage = pathname === "/chapter";
-
+  const isCoursePage = pathname === "/courses";
   const isSearchPage = pathname === "/search";
+
+
   return (
     <>
-          {isSearchPage && (
+      {isSearchPage && (
         <div className="hidden md:block">
           <SearchInput />
         </div>
       )}
-
       <div className="flex gap-x-2 ml-auto">
-        {isTeacherPage || isPlayerPage ? (
+        {isTeacherPage || isCoursePage ? (
           <Link href="/">
             <Button size="sm" variant="ghost">
               <LogOut className="h-4 w-4 mr-2" />
               Exit
             </Button>
-           </Link>
+          </Link>
         ) :  (
           <Link href="/teacher/courses">
             <Button size="sm" variant="ghost">
